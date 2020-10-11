3831ed5a8cfdcbed28e6e1b736351c22ef1b1356
chenxuewen01 <chenxuewen01@bilibili.com>
2020-10-11 17:04:30 +0800

    add

---
+++ rust-variable/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // bool
+    let mybool: bool = true;
+    println!("bool = {}", mybool);
+
+    // char
+    let mychar: char = 'a';
+    println!("char = {}", mychar);
+
+    // number i8 i16 i32 i64 u8 u16 u32 u64 f32 f64
+    let mynumber: i8 = 10;
+    println!("number = {}", mynumber);
+
+    // 自适应类型
+}
