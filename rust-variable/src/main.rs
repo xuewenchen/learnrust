bd69d3f7675016410f2abf1dbce0fe1ac7ce06d1
chenxuewen01 <chenxuewen01@bilibili.com>
2020-10-11 20:12:22 +0800

    add variable

--- rust-variable/src/main.rs
+++ rust-variable/src/main.rs
@@ -11,5 +11,31 @@ fn main() {
     let mynumber: i8 = 10;
     println!("number = {}", mynumber);
 
-    // 自适应类型
+    // 自适应类型, 和机器有关
+    println!("size = {}", usize::max_value());
+
+    // 数组
+    // [Type; size]
+    let arr: [u32; 5] = [1, 2, 3, 4, 5];
+    println!("arrary[0] = {}", arr[0]);
+
+    // 元祖
+    let mytuple: (i8, f32, char) = (1, 1.2, 'h');
+    println!("mytuple1 = {}", mytuple.0);
+    println!("mytuple2 = {}", mytuple.1);
+    println!("mytuple3 = {}", mytuple.2);
+    // 元祖拆解
+    let (x, y, z) = mytuple;
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
+
+    // 数组传参
+    show(arr);
+}
+
+fn show(arr: [u32; 5]) {
+    for i in &arr {
+        println!("{}", i);
+    }
 }
